62b71ab6e72da796bdd7a402c766e43db928b4c8
NAKIVO, Inc <github@nakivo.com>
2019-04-10 12:17:52 +0300

    NAKIVO Backup & Replication 8.5 (Beta)

---
+++ README.md
@@ -0,0 +1,9 @@
+# NAKIVO Backup & Replication Transporter
+
+This project contains only Transporter component of NAKIVO Backup and Replication and requires a separate full installation of NAKIVO Backup and Replication to work correctly.
+
+## Support
+
+```
+support@nakivo.com
+```
---
+++ post_install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+PRODUCT='NAKIVO Backup & Replication'
+URL="https://d96i82q710b04.cloudfront.net/res/product/beta/NAKIVO_Transporter_Installer_8.5.0-BETA.sh"
+SHA256="7c8a762df89b46222e5e80d14e12cce1aeb92c7f108de8bd96917578df52aed8"
+
+PRODUCT_ROOT="/usr/local/nakivo"
+INSTALL="inst.sh"
+
+curl --fail --tlsv1.2 -o $INSTALL $URL
+if [ $? -ne 0 -o ! -e $INSTALL ]; then
+    echo "ERROR: Failed to get $PRODUCT installer"
+    rm $INSTALL >/dev/null 2>&1
+    exit 1
+fi
+
+CHECKSUM=`sha256 -q $INSTALL`
+if [ "$SHA256" != "$CHECKSUM" ]; then
+    echo "ERROR: Incorrect $PRODUCT installer checksum"
+    rm $INSTALL >/dev/null 2>&1
+    exit 2
+fi
+
+sh ./$INSTALL -s -i "$PRODUCT_ROOT" --eula-accept 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: $PRODUCT install failed"
+    rm $INSTALL >/dev/null 2>&1
+    exit 3
+fi
+rm $INSTALL >/dev/null 2>&1
+
+exit 0
